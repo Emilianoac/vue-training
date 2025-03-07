9e7e876a17611254736946e019511d0eb2be3f1b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-07 13:43:13 -0300

    TEST: Implement tests for LangToggleComponent to handle dropdown close on outside click

--- src/__tests__/components/LangToggleComponent.test.ts
+++ src/__tests__/components/LangToggleComponent.test.ts
@@ -1,7 +1,8 @@
 import { mount } from "@vue/test-utils";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { createI18n } from "vue-i18n";
 import LangSwitcher from "../../components/LangToggleComponent.vue";
+import { nextTick } from "vue";
 
 const i18n = createI18n({
   legacy: false,
@@ -56,5 +57,47 @@ describe("Lang Toggle component", () => {
   
     expect(i18n.global.locale.value).toBe("es"); 
   });
+
+  describe("when clicking outside the dropdown menu", () => {
+    it("should close the dropdown when clicking outside", async () => {
+      const wrapper = mount(LangSwitcher, {
+        global: { plugins: [i18n] },
+      });
+      const langToggleMenu = wrapper.find("[data-test='lang-toggle-menu']");
+      const langToggleButton = wrapper.find("[data-test='lang-toggle-button']");  
   
+      await langToggleButton.trigger("click");
+      document.dispatchEvent(new MouseEvent("mousedown", {bubbles: true}));
+      await nextTick();
+  
+      expect(langToggleMenu.classes()).toContain("hidden");
+    });
+
+    it("should not close the dropdown when clicking inside", async () => {
+      const wrapper = mount(LangSwitcher, {
+        global: { plugins: [i18n] },
+      });
+      const langToggleMenu = wrapper.find("[data-test='lang-toggle-menu']");
+      const langToggleButton = wrapper.find("[data-test='lang-toggle-button']");
+  
+      await langToggleButton.trigger("click");
+      await langToggleMenu.trigger("click");
+
+      expect(langToggleMenu.classes()).not.toContain("hidden");
+    });
+
+    it("should add and remove the event listener", () => {
+      const addEventListenerSpy = vi.spyOn(document, "addEventListener");
+      const removeEventListenerSpy = vi.spyOn(document, "removeEventListener");
+      const wrapper = mount(LangSwitcher, {
+        global: { plugins: [i18n] },
+      });
+  
+      expect(addEventListenerSpy).toHaveBeenCalledWith("mousedown", expect.any(Function));
+  
+      wrapper.unmount();
+  
+      expect(removeEventListenerSpy).toHaveBeenCalledWith("mousedown", expect.any(Function));
+    });
+  });
 });
