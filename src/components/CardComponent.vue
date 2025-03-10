d4736eba0f0d6b534d3009f4e988a46c215c650b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-10 10:02:09 -0300

    FEAT: Add CardComponent with basic structure and styling

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <card class="bg-white dark:bg-slate-900 shadow-md rounded-lg hover:opacity-95 hover:shadow-lg dark:hover:shadow-xl transition-all duration-300 overflow-hidden">
+    <slot />
+  </card>
+</template>
+
+<style lang="postcss" scoped>
+  
+</style>
