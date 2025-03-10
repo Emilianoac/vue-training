c9785f40107e9ad89bc403340e4962ef99abfcb7
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-10 10:04:17 -0300

    FEAT: Add QuizCardComponent and QuizListComponent for quiz display

---
+++ src/components/quiz/QuizCardComponent.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { useGeneralStore } from "@/stores/general";
+import BadgeComponent from "@/components/BadgeComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
+
+const store = useGeneralStore();
+
+defineProps<{
+  name: {
+    es: string;
+    en: string;
+  },
+  level: "easy" | "medium" | "hard";
+  levelLabel: {
+    es: "Fácil" | "Medio" | "Difícil";
+    en: "Easy" | "Medium" | "Hard";
+  },
+  id: string;
+  img: string;
+  description: {
+    es: string;
+    en: string;
+  };
+}>();
+</script>
+
+<template>
+  <CardComponent>
+    <router-link class="p-4 block" :to="`/quizzes/${id}`">
+      <div class="w-[50px] h-[50px] bg-slate-200 dark:bg-slate-800 p-1 rounded-full flex justify-center items-center">
+        <img :src="img" alt="Challenge Image" class="w-[30px]">
+      </div>
+      <div class="mt-3">
+        <h3 class="font-bold tex">{{ name[store.locale] }}</h3>
+        <BadgeComponent 
+          :text="levelLabel[store.locale]" 
+          :type="level"
+          class="mt-4"
+         />
+      </div>
+      <p class="mt-3 text-sm text-gray-500 dark:text-gray-400">{{ description[store.locale] }}</p>
+    </router-link>
+  </CardComponent>
+</template>
+
+<style lang="postcss">
+  
+</style>
---
+++ src/components/quiz/QuizListComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import quizzes from "@/data/quizzes";
+import QuizCardComponent from "@/components/quiz/QuizCardComponent.vue";
+
+</script>
+
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
+    <QuizCardComponent
+      v-for="quiz in quizzes" 
+      :key="quiz.id" 
+      :name="quiz.title" 
+      :level="quiz.level" 
+      :level-label="quiz.levelLabel"
+      :img="quiz.category.image"
+      :id="quiz.id" 
+      :description="quiz.description"
+    />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+  
+</style>
