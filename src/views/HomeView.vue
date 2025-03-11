596d5ff90afb6573c46c4318e19ed88e697419ea
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-11 20:20:32 -0300

    STYLE: Update HomeView layout and button styles for improved consistency.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,21 +7,21 @@ import QuizListComponent from "@/components/quiz/QuizListComponent.vue";
 
 <template>
     <!-- Header -->
-    <section class="block md:flex  items-center justify-between my-14 md:my-28">
+    <section class="block md:flex  items-center justify-between my-14 md:my-14">
       <div class="mb-10 md:mb-0">
         <h1 class="text-4xl md:text-6xl font-bold max-w-[540px]">
           {{ $t("home.welcome") }} 
-          <span class="text-color-primary font-extrabold">Vue.js</span>
+          <span>Vue.js</span>
         </h1>
         <p class="text-lg mt-5 max-w-[480px]">
           {{ $t("home.description") }}
         </p>
-        <div class="mt-5">
-          <router-link to="/quizzes" class="bg-slate-800 hover:bg-slate-700 text-white px-6 py-3 rounded-full mt-4 inline-block text-sm">
+        <div class="mt-5 gap-4 flex">
+          <router-link to="/quizzes" class="app-button primary">
            {{ $t("general.quiz.many") }}
           </router-link>
 
-          <router-link to="/challenges" class="bg-slate-800 hover:bg-slate-700 text-white px-6 py-3 rounded-full mt-4 ml-4 inline-block text-sm">
+          <router-link to="/challenges" class="app-button primary">
             {{ $t("general.challenge.many") }}
           </router-link>
         </div>
