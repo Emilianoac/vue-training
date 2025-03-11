5e387ff641da73f8dd26f3d8910bcb7dcb7fcbdf
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-11 20:58:13 -0300

    FEAT: Add quiz localization support in English and Spanish

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,9 +1,11 @@
 import general from "./general.json";
 import home from "./home.json";
 import challenge from "./challenge.json";
+import quiz from "./quiz.json";
 
 export default {
   general,  
   home,
-  challenge
+  challenge,
+  quiz
 };
