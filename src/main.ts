a4a573bd0012d0364869ebcda7374748d25ac4e5
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-11 20:54:07 -0300

    FEAT: Add Highlight.js and Vue plugin for syntax highlighting support.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { i18n } from "./i18n";
+import "highlight.js/styles/stackoverflow-dark.css";
+import "highlight.js/lib/common";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(hljsVuePlugin);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
