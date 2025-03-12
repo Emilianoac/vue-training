e3c4b976389bba4db1c27bbd52f064eaf012735b
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-12 20:34:24 -0300

    STYLE: center content in hero section on small devices.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,17 @@ import HeaderIllustration from "@/components/HeaderIllustration.vue";
 </script>
 
 <template>
-    <!-- Header -->
+    <!-- Hero Section -->
     <section class="block lg:flex items-center justify-between my-14 lg:my-14">
-      <div class="mb-10 md:mb-0">
-        <h1 class="text-4xl md:text-6xl font-bold max-w-[540px]">
+      <div class="mb-10 md:mb-0 text-center lg:text-left">
+        <h1 class="text-4xl md:text-6xl font-bold max-w-[560px] mx-auto lg:mx-0">
           {{ $t("home.welcome") }} 
-          <span>Vue.js</span>
+          Vue.js
         </h1>
-        <p class="text-lg mt-5 max-w-[480px]">
+        <p class="text-lg mt-5 max-w-[480px] mx-auto lg:mx-0">
           {{ $t("home.description") }}
         </p>
-        <div class="mt-5 gap-4 flex">
+        <div class="mt-5 gap-4 flex justify-center lg:justify-start">
           <router-link to="/quizzes" class="app-button primary">
            {{ $t("general.quiz.many") }}
           </router-link>
@@ -33,13 +33,13 @@ import HeaderIllustration from "@/components/HeaderIllustration.vue";
 
     <!-- Challenges -->
     <section class="mt-20">
-      <h2 class="text-3xl font-bold text-center">{{ $t("general.challenge.many") }}</h2>
+      <h2 class="text-2xl md:text-3xl font-bold text-center">{{ $t("general.challenge.many") }}</h2>
       <ChallengeListComponent />
     </section>
 
     <!-- Quizzes -->
     <section class="mt-20">
-      <h2 class="text-3xl font-bold text-center">{{ $t("general.quiz.many") }}</h2>
+      <h2 class="text-2xl md:text-3xl font-bold text-center">{{ $t("general.quiz.many") }}</h2>
       <QuizListComponent />
     </section>
 
