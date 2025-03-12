b06f09559a48b60638c0c9cfc2c89f218daade95
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-12 19:03:52 -0300

    STYLE: Refactor MobileNavbarComponent for improved layout and add active styles.

--- src/components/global/MobileNavbarComponent.vue
+++ src/components/global/MobileNavbarComponent.vue
@@ -1,17 +1,22 @@
 <script lang="ts" setup>
 
 const menuItems = [
-  { title: "general.home", path: "/" },
   { title: "general.challenge.many", path: "/challenges" },
+  { title: "general.home", path: "/" },
   { title: "general.quiz.many", path: "/quizzes" },
 ]
 </script>
 
 <template>
-  <nav class="bg-slate-100 dark:bg-slate-900 text-black dark:text-white rounded-md shadow-md bottom-3 mx-6 sticky block md:hidden">
-    <div class="flex items-center justify-between container mx-auto p-4">
-      <router-link v-for="(menuItem, index) in menuItems" :key="index" :to="menuItem.path" class="hover:underline">
-        {{ $t(menuItem.title) }}
+  <nav class="bg-white dark:bg-slate-900 text-black dark:text-white rounded-md shadow-md bottom-3 mx-6 fixed right-0 left-0 block md:hidden">
+    <div class="grid grid-cols-3 items-center justify-between container mx-auto p-3">
+      <router-link 
+        v-for="(menuItem, index) in menuItems" 
+        :key="index" 
+        :to="menuItem.path" 
+        active-class="text-color-primary font-semibold"
+        class="text-[0.95em] text-center py-1 border-b-[3px] border-r border-l first-of-type:border-0 last-of-type:border-0 border-b-transparent block w-full">
+          {{ $t(menuItem.title) }}
       </router-link>
     </div>
   </nav>
