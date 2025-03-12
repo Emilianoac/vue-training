00284cd62a37dfc7b52049ab112963bd8829875d
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-12 18:24:43 -0300

    STYLE: Simplify NotFoundView styling by removing redundant classes and updating button styles

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -5,9 +5,9 @@
 <template>
   <div class="flex items-center justify-center h-[calc(100vh-200px)]">
     <div class="text-center">
-      <h1 class="text-6xl md:text-9xl font-bold text-color-primary">404</h1>
+      <h1 class="text-6xl md:text-9xl font-bold">404</h1>
       <p class="text-xl md:text-2xl mt-4">{{ $t("general.pageNotFound") }}</p>
-      <router-link to="/" class="mt-4 inline-block bg-blue-600 rounded-full px-6 py-2 text-white">
+      <router-link to="/" class="app-button primary mt-8">
         {{ $t("general.goToHome") }}
       </router-link>
     </div>
