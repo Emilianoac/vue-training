d50b87d26c7d0f609e8413cdc71c09c63626e601
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-13 16:16:10 -0300

    UPDATE: Rename quiz import and add a new quiz

--- src/data/quizzes/index.ts
+++ src/data/quizzes/index.ts
@@ -1,7 +1,10 @@
-import vue3Basics from "./general/vue-3-basics";
+import vue_3_basics from "./general/vue-3-basics";
+import routes_and_navigation_with_vue_router from "./general/routes-and-navigation-with-vue-router";
 
 const quizzes = [
-  vue3Basics,
+  vue_3_basics,
+  routes_and_navigation_with_vue_router
 ];
 
 export default quizzes;
+
