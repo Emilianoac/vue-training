a113a4116531d7be5568f9a6eb18e2564b7db30a
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-03-14 16:51:46 -0300

    UPDATE: Refactor ChallengeListComponent and QuizListComponent to use CardComponent for rendering

--- src/components/challenge/ChallengeListComponent.vue
+++ src/components/challenge/ChallengeListComponent.vue
@@ -1,20 +1,22 @@
 <script lang="ts" setup>
 import challenges from "@/data/challenges";
-import ChallengeCardComponent from "@/components/challenge/ChallengeCardComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
 
 </script>
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
-    <ChallengeCardComponent 
+    <CardComponent
       v-for="challenge in challenges" 
       :key="challenge.id" 
-      :name="challenge.title" 
+      :title="challenge.title" 
       :level="challenge.level" 
       :level-label="challenge.levelLabel"
       :img="challenge.cover"
       :description="challenge.short_description"
       :id="challenge.id" 
+      type="challenge"
+      link="/challenge/:id"
     />
   </div>
 </template>
--- src/components/quiz/QuizListComponent.vue
+++ src/components/quiz/QuizListComponent.vue
@@ -1,19 +1,21 @@
 <script lang="ts" setup>
 import quizzes from "@/data/quizzes";
-import QuizCardComponent from "@/components/quiz/QuizCardComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
 
 </script>
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
-    <QuizCardComponent
+    <CardComponent
       v-for="quiz in quizzes" 
       :key="quiz.id" 
-      :name="quiz.title" 
+      :title="quiz.title" 
       :level="quiz.level" 
       :level-label="quiz.levelLabel"
       :img="quiz.category.image"
       :id="quiz.id" 
+      type="quiz"
+      link="/quiz/:id"
       :description="quiz.description"
     />
   </div>
