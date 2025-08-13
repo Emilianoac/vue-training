cda943f18ddf09e0ebe3458a1a616fb868dbc155
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-08-13 14:32:55 -0400

    style: refactor layout and typography in HomeView for improved readability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,21 +8,21 @@ import SwiperComponent from "@/components/SwiperComponent.vue";
 
 <template>
   <!-- Hero Section -->
-  <section class="block lg:flex items-center justify-between my-14 lg:my-14">
-    <div class="mb-10 md:mb-0 text-center lg:text-left">
-      <h1 class="text-4xl md:text-6xl font-bold max-w-[560px] mx-auto lg:mx-0">
+  <section class="block lg:grid grid-cols-[1fr_0.6fr] items-center justify-between my-14 lg:my-14">
+    <div class="mb-10 md:mb-0">
+      <h1 class="text-5xl md:text-6xl !leading-tight font-bold mx-auto lg:mx-0">
         {{ $t("home.welcome") }} 
         Vue.js
       </h1>
-      <p class="text-lg mt-5 max-w-[480px] mx-auto lg:mx-0">
+      <p class="text-2xl my-10  max-w-[500px]">
         {{ $t("home.description") }}
       </p>
-      <div class="mt-5 gap-4 flex justify-center lg:justify-start">
-        <router-link to="/quizzes" class="app-button primary">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:w-max gap-4 justify-center lg:justify-start">
+        <router-link to="/quizzes" class="app-button primary px-8">
           {{$t("general.goTo")}} {{ $t("general.quiz.many") }}
         </router-link>
 
-        <router-link to="/challenges" class="app-button primary">
+        <router-link to="/challenges" class="app-button-outline px-8">
           {{$t("general.goTo")}} {{$t("general.challenge.many") }}
         </router-link>
       </div>
