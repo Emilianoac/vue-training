272cec5486dd4cb86c204cb05c344ae66ea89227
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-08-15 02:40:28 -0400

    feat(ChallengeListComponent, QuizListComponent): adjust grid layout for improved responsiveness

--- src/components/challenge/ChallengeListComponent.vue
+++ src/components/challenge/ChallengeListComponent.vue
@@ -8,7 +8,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
     <CardComponent
       v-for="challenge in challenges" 
       :key="challenge.id" 
--- src/components/quiz/QuizListComponent.vue
+++ src/components/quiz/QuizListComponent.vue
@@ -9,7 +9,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
     <CardComponent
       v-for="quiz in quizzes" 
       :key="quiz.id" 
