19e016e29de4df2211f2f5a56c21f5ce7b56bed1
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-12-30 17:26:19 -0300

    style: add a min height to the default layout.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@ import MobileNavbarComponent from "@/components/layout/MobileNavbarComponent.vue
 
 <template>
   <HeaderComponent/>
-  <div class="container max-w-[1280px] mx-auto px-4 md:px-10 mb-20">
+  <div class="container max-w-[1280px] mx-auto px-4 md:px-10 mb-20 min-h-[80vh]">
     <slot />
   </div>
   <MobileNavbarComponent/>
