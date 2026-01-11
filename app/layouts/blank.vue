920b2f05248b2e4a39c4d185447e24784e12db9d
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-01-11 14:13:02 -0300

    feat(ui): add a new layout without header and footer

---
+++ app/layouts/blank.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  
+</script>
+
+<template>
+  <div class="container max-w-[1280px] mx-auto px-4 md:px-10 mb-20 min-h-[80vh]">
+    <slot />
+  </div>
+</template>
+
+<style lang="postcss">
+</style>
