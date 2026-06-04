506b1072fc272da05c14614c2ababbab636acd9f
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-06-04 17:34:12 -0400

    refactor(routes): remove legacy activity index redirects

--- app/pages/challenges/index.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts">
-await navigateTo("/learn/challenges", { replace: true, redirectCode: 301 });
-</script>
-
-<template></template>
--- app/pages/quizzes/index.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts">
-await navigateTo("/learn/quizzes", { replace: true, redirectCode: 301 });
-</script>
-
-<template></template>
