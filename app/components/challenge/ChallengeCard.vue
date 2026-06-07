ee4ecefbd1d4ea5f4f0847baf6597e2fab0ad602
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-06-07 14:34:45 -0400

    style(challenge, quiz): improve cards layout

--- app/components/challenge/ChallengeCard.vue
+++ app/components/challenge/ChallengeCard.vue
@@ -2,7 +2,8 @@
 import type { Challenge } from "@/schemas/challenge.schema";
 
 import ChipComponent from "@/components/ui/ChipComponent.vue";
-import CardComponent from "@/components/ui/CardComponent.vue";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 defineProps<{
   challenge: Challenge;
@@ -10,35 +11,43 @@ defineProps<{
 </script>
 
 <template>
-  <CardComponent>
-    <NuxtLink class="block" :to="`/challenges/${challenge.slug}`" external>
-      <div class="relative">
-        <!-- Image -->
-        <img
-          :src="challenge.cover.url"
-          alt="Challenge Image"
-          class="w-full h-48 object-cover object-center"
-        />
-        <!-- Level Chip -->
-        <ChipComponent
-          :text="$t(`general.levels.${challenge.level}`)"
-          :type="challenge.level"
-          class="absolute top-3 right-3"
-        />
-      </div>
-      <div class="p-4">
-        <!-- Title -->
-        <h3 class="text-sm font-bold line-clamp-1">{{ challenge.title }}</h3>
-        <!-- Description -->
-        <p
-          class="text-sm text-gray-500 dark:text-gray-400 line-clamp-2 mt-3"
-          :title="challenge.short_description"
-        >
-          {{ challenge.short_description }}
-        </p>
-      </div>
-    </NuxtLink>
-  </CardComponent>
+  <Card class="p-0 hover:bg-card/80">
+    <Button
+      as-child
+      variant="link"
+      class="h-auto w-full block p-0 whitespace-break-spaces rounded-xl text-foreground decoration-0"
+    >
+      <NuxtLink class="block" :to="`/challenges/${challenge.slug}`" external>
+        <CardContent class="px-0">
+          <div class="relative overflow-hidden rounded-tr-xl rounded-tl-xl">
+            <!-- Image -->
+            <img
+              :src="challenge.cover.url"
+              alt="Challenge Image"
+              class="w-full h-48 object-cover object-center"
+            />
+            <!-- Level Chip -->
+            <ChipComponent
+              :text="$t(`general.levels.${challenge.level}`)"
+              :type="challenge.level"
+              class="absolute top-3 right-3"
+            />
+          </div>
+          <div class="p-4">
+            <!-- Title -->
+            <h3 class="text-sm font-bold line-clamp-1">{{ challenge.title }}</h3>
+            <!-- Description -->
+            <p
+              class="text-sm text-gray-500 dark:text-gray-400 line-clamp-2 mt-3"
+              :title="challenge.short_description"
+            >
+              {{ challenge.short_description }}
+            </p>
+          </div>
+        </CardContent>
+      </NuxtLink>
+    </Button>
+  </Card>
 </template>
 
 <style lang="postcss" scoped></style>
--- app/components/quiz/QuizCard.vue
+++ app/components/quiz/QuizCard.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
+import type { QuizListItem } from "@/schemas/quiz.schema";
+
 import ChipComponent from "@/components/ui/ChipComponent.vue";
 import { Card, CardContent } from "@/components/ui/card";
-
-import type { QuizListItem } from "@/schemas/quiz.schema";
+import { Button } from "@/components/ui/button";
 
 defineProps<{
   quiz: QuizListItem;
@@ -10,34 +11,40 @@ defineProps<{
 </script>
 
 <template>
-  <Card class="p-0">
-    <NuxtLink class="block" :to="`/quizzes/${quiz.slug}`">
-      <CardContent class="px-4">
-        <div class="flex justify-between items-start py-4 pb-0">
-          <!-- Image -->
-          <div
-            class="w-[50px] h-[50px] bg-slate-200 dark:bg-slate-800 p-1 rounded-full flex justify-center items-center"
-          >
-            <img :src="quiz.category.image.url" alt="Quiz Image" class="w-[30px]" />
+  <Card class="p-0 hover:bg-card/80">
+    <Button
+      as-child
+      variant="link"
+      class="h-auto w-full block p-0 whitespace-break-spaces rounded-xl text-foreground decoration-0"
+    >
+      <NuxtLink class="block" :to="`/quizzes/${quiz.slug}`">
+        <CardContent class="px-4">
+          <div class="flex justify-between items-start py-4 pb-0">
+            <!-- Image -->
+            <div
+              class="w-12.5 h-12.5 bg-slate-200 dark:bg-slate-800 p-1 rounded-full flex justify-center items-center"
+            >
+              <img :src="quiz.category.image.url" alt="Quiz Image" class="w-7.5" />
+            </div>
+            <!-- Level Chip -->
+            <ChipComponent :text="$t(`general.levels.${quiz.level}`)" :type="quiz.level" />
           </div>
-          <!-- Level Chip -->
-          <ChipComponent :text="$t(`general.levels.${quiz.level}`)" :type="quiz.level" />
-        </div>
-        <div class="p-4">
-          <!-- Category -->
-          <p class="text-sm opacity-70 mb-1">{{ quiz.category.name }}</p>
-          <!-- Title -->
-          <h3 class="text-sm font-bold line-clamp-1">{{ quiz.title }}</h3>
-          <!-- Description -->
-          <p
-            class="text-sm text-gray-500 dark:text-gray-400 line-clamp-2 mt-3"
-            :title="quiz.description"
-          >
-            {{ quiz.description }}
-          </p>
-        </div>
-      </CardContent>
-    </NuxtLink>
+          <div class="p-4">
+            <!-- Category -->
+            <p class="text-sm opacity-70 mb-1">{{ quiz.category.name }}</p>
+            <!-- Title -->
+            <h3 class="text-sm font-bold line-clamp-1">{{ quiz.title }}</h3>
+            <!-- Description -->
+            <p
+              class="text-sm text-gray-500 dark:text-gray-400 line-clamp-2 mt-3"
+              :title="quiz.description"
+            >
+              {{ quiz.description }}
+            </p>
+          </div>
+        </CardContent>
+      </NuxtLink>
+    </Button>
   </Card>
 </template>
 
