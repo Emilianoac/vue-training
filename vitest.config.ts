2c166b48853cc4cb6d8ad99bf7a83038b07bbe90
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-07-03 14:36:59 -0400

    test(config): reorganize test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,31 @@
+import { fileURLToPath } from "node:url";
 import { defineVitestProject } from "@nuxt/test-utils/config";
-import { configDefaults } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
-export default await defineVitestProject({
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./app", import.meta.url)),
+      "~": fileURLToPath(new URL("./app", import.meta.url)),
+    },
+  },
   test: {
     globals: true,
-    environment: "nuxt",
-    setupFiles: [
-      "./setupTests.ts"
-    ],
-    exclude: [
-      ...configDefaults.exclude,
-      "e2e/*"
+    projects: [
+      {
+        test: {
+          name: "unit",
+          include: ["_tests_/unit/**/*.{test,spec}.ts"],
+          environment: "node",
+        },
+      },
+      await defineVitestProject({
+        test: {
+          name: "integration",
+          include: ["_tests_/integration/**/*.{test,spec}.ts"],
+          environment: "nuxt",
+        },
+      }),
     ],
   },
-})
+});
